a3dc3bf51ecadcea4c9f625632d7ae247586d648
Oliverillo <omm0004@alu.medac.es>
2025-11-17 18:24:54 +0100

    ejerMusica

--- ListasEjer/app/src/main/java/com/example/listasejer/FilmsAdapter.java
+++ ListasEjer/app/src/main/java/com/example/listasejer/FilmsAdapter.java
@@ -11,15 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class FilmsAdapter extends RecyclerView.Adapter<FilmsAdapter.FilmViewHolder> {
-
     private List<Films> listaPeliculas;
     private Context context;
-
     public FilmsAdapter(Context context, List<Films> listaPeliculas) {
         this.context = context;
         this.listaPeliculas = listaPeliculas;
     }
-
     @NonNull
     @Override
     public FilmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -27,32 +24,25 @@ public class FilmsAdapter extends RecyclerView.Adapter<FilmsAdapter.FilmViewHold
                 .inflate(R.layout.item_film, parent, false);
         return new FilmViewHolder(itemView);
     }
-
     @Override
     public void onBindViewHolder(@NonNull FilmViewHolder holder, int position) {
         Films film = listaPeliculas.get(position);
         holder.bindFilm(film);
     }
-
     @Override
     public int getItemCount() {
         return listaPeliculas.size();
     }
-
     public class FilmViewHolder extends RecyclerView.ViewHolder {
-
         ImageView imageView;
         TextView titleTextView;
-
         public FilmViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.imageView);
             titleTextView = itemView.findViewById(R.id.textView);
         }
-
         public void bindFilm(Films film) {
             titleTextView.setText(film.getTitle());
-
             int resId = context.getResources().getIdentifier(
                     film.getImage(),
                     "drawable",
